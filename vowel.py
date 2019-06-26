e7fc1a0de5d67d0d6db88e725a2bd1bdec30ec43
pottriselvam <51950292+pottriselvam@users.noreply.github.com>
2019-06-27 00:06:42 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,6 +1,5 @@
 s=input()
-d='aeiouAEIOU'
-K='bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
+d=['a','e','i','o','u','A','E','I','O','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
 if s in d:
  print("Vowel")
 elif s in k:
